de091f49b3d36911c992d57cb37173f68aaad49e
Ialcantara09 <ivone_alcantara@yahoo.com.br>
2026-03-24 23:10:46 +0000

    testando sem order-result-id e order-result-status

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -11,13 +11,19 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
 // Act
-  await page.getByTestId('search-order-id').fill('VLO-T2VAZ8')
-  await page.getByTestId('search-order-button').click()
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-T2VAZ8')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
 // Assert  
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-T2VAZ8')
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  //await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
+  //await expect(page.getByTestId('order-result-id')).toContainText(' ')
+  await expect(page.getByText('VLO-T2VAZ8')).toBeVisible({timeout: 10_000})
+  await expect(page.getByText('VLO-T2VAZ8')).toContainText(' ')  
+
+  //await expect(page.getByTestId('order-result-status')).toBeVisible()
+  //await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByText('APROVADO')).toContainText(' ')
+
 })
 
